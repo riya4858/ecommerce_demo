937c8cde6ae3d87679127cc7b59fa8b6cc0efb73
riya4858 <riyanp71@gmail.com>
2023-11-10 17:03:15 +0530

    first

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,2 +1,25 @@
 from django.db import models
 
+class Department(models.Model):
+    dept_name=models.CharField(max_length=100)
+    dept_desc=models.CharField(max_length=100)
+
+class Doctor(models.Model):
+    name=models.CharField(max_length=100)
+    username=models.CharField(max_length=100,null=True,blank=True)
+    password=models.CharField(max_length=100,null=True,blank=True)
+    image=models.ImageField(upload_to='photos/doctors',blank=True)
+    mobile=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+
+class Patient(models.Model):
+    name=models.CharField(max_length=100)
+    username=models.CharField(max_length=100,null=True,blank=True)
+    password=models.CharField(max_length=100,null=True,blank=True)
+    image=models.ImageField(upload_to='photos/doctors',blank=True)
+    symptoms=models.CharField(max_length=100,null=True,blank=True)
+    mobile=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    dob=models.DateField()
+    gender=models.CharField(max_length=100)
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
+from .models import *
+import secrets
+import string
+
 def admin(request):
     if request.method == 'POST':
         username=request.POST.get('username')
@@ -14,11 +18,90 @@ def admin(request):
 def dashboard(request):
     return render(request,'dashboard.html')
 
+def add_department(request):
+    if request.method == 'POST':
+        dept_name=request.POST.get('dept_name')
+        dept_desc=request.POST.get('dept_desc')
+        dept=Department(dept_name=dept_name,dept_desc=dept_desc)
+        dept.save()
+    dep=Department.objects.all()
+    return render(request,'add_department.html',{'dep':dep})
+
+def edit_dept(request,pk):
+    dept=Department.objects.get(id=pk)
+    return render(request,'editdepartment.html',{'dept':dept})
+
+def delete_dept(request,pk):
+    dept=Department.objects.get(id=pk)
+    dept.delete()
+    return redirect('add_department')
+
+def generate_password(length=6):
+    characters = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
 def doctor(request):
-    return render(request,'doctor.html')
+    
+    dept=Department.objects.all()
+    doc=Doctor.objects.all()
+    return render(request,'doctor.html',{'dept':dept,'doc':doc})
+
+
+def adddoctor(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        username=request.POST.get('username')
+        image = request.FILES.get('image')
+        mobile=request.POST.get('mobile')
+        address=request.POST.get('address')
+        dept_id=request.POST.get('dept_name')
+        dept_name=Department.objects.get(id=dept_id)
+        password=generate_password()
+        if image:
+            doc=Doctor(name=name,image=image,mobile=mobile,address=address,department=dept_name,
+                       username=username,password=password)
+            doc.save()
+        return redirect('doctor')
+    
+def edit_doc(request,pk):
+    doc=Doctor.objects.get(id=pk)
+    return render(request,'editdoctor.html',{'doc':doc})
+
+def delete_doc(request,pk):
+    dept=Doctor.objects.get(id=pk)
+    dept.delete()
+    return redirect('doctor')
 
 def patient(request):
-    return render(request,'patient.html')
+    pat=Patient.objects.all()
+    return render(request,'patient.html',{'pat':pat})
+
+def addpatient(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        username=request.POST.get('username')
+        gender=request.POST.get('gender')
+        dob=request.POST.get('dob')
+        image = request.FILES.get('image')
+        symptoms=request.POST.get('symptoms')
+        mobile=request.POST.get('mobile')
+        address=request.POST.get('address')
+        password=generate_password()
+        if image:
+                doc=Patient(name=name,image=image,mobile=mobile,address=address,password=password,
+                        username=username,symptoms=symptoms,gender=gender,dob=dob)
+                doc.save()
+        return redirect('patient')
+    
+def edit_patient(request,pk):
+    pat=Patient.objects.get(id=pk)
+    return render(request,'editpatient.html',{'pat':pat})
+
+def delete_patient(request,pk):
+    pat=Patient.objects.get(id=pk)
+    pat.delete()
+    return redirect('patient')
 
 def appointment(request):
     return render(request,'appointment.html')
